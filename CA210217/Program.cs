75ce9bb55c6df75514f0f43a230ce04527efda40
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-02-17 19:02:29 +0100

    re-implement 1st fadd 1st line

--- CA210217/Program.cs
+++ CA210217/Program.cs
@@ -8,14 +8,28 @@ namespace CA210217
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Lakcim();
+            Console.ReadKey();
         }
 
         private static void Lakcim()
         {
-            
+            Console.Write("irányítószám: ");
+            string iranyitoSzam = Console.ReadLine();
+            Console.Write("település neve: ");
+            string telepulesNeve = Console.ReadLine();
+            Console.Write("kozterület neve: ");
+            string kozteruletNeve = Console.ReadLine();
+            Console.Write("közterület jellege: ");
+            string kozteruletJellege = Console.ReadLine();
+            Console.Write("házszám: ");
+            string hazszam = Console.ReadLine();
+
+            Console.WriteLine(iranyitoSzam + " " + telepulesNeve + " " + kozteruletNeve + " " + kozteruletJellege + " " + hazszam + ".");
+            Console.WriteLine("{0} {1} {2} {3} {4}.", iranyitoSzam, telepulesNeve, kozteruletNeve, kozteruletJellege, hazszam);
+            Console.WriteLine($"{iranyitoSzam} {telepulesNeve} {kozteruletNeve} {kozteruletJellege} {hazszam}.");
         }
     }
 }
