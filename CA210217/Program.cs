15b55b0466443dd03cc1e388f22c99997b2effc1
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-02-17 18:53:35 +0100

    Add project files.

---
+++ CA210217/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA210217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lakcim();
+        }
+
+        private static void Lakcim()
+        {
+            
+        }
+    }
+}
